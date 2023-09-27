a98813e8eee959739cd520bf03cf9648b31996df
tienhuynh-tn <tien.huynhlt.tn@gmail.com>
2023-09-27 23:43:02 +0700

    [tienhuynh-tn] Constant

---
+++ server/src/main/java/com/shecodes2023/h2o/server/constant/DefaultSortPropertyConstant.java
@@ -0,0 +1,7 @@
+package com.shecodes2023.h2o.server.constant;
+
+public class DefaultSortPropertyConstant {
+    public static final String ID = "Id";
+    public static final String CREATED_DATE = "CreatedDate";
+    public static final String MODIFIED_DATE = "ModifiedDate";
+}
---
+++ server/src/main/java/com/shecodes2023/h2o/server/constant/StatusConstant.java
@@ -0,0 +1,6 @@
+package com.shecodes2023.h2o.server.constant;
+
+public class StatusConstant {
+    public static final String ACTIVE = "Đang hoạt động";
+    public static final String IN_ACTIVE = "Không hoạt động";
+}

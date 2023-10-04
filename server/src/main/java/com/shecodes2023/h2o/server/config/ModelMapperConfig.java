46f6f9df59ac428889eb7a817ae6da67864e9b62
tienhuynh-tn <tien.huynhlt.tn@gmail.com>
2023-10-04 21:12:29 +0700

    [tienhuynh-tn] Config Model Mapper

---
+++ server/src/main/java/com/shecodes2023/h2o/server/config/ModelMapperConfig.java
@@ -0,0 +1,17 @@
+package com.shecodes2023.h2o.server.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
+}

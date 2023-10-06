ab37a2ff28026e750b6db98a887119e13a48c035
khanhleeee <khanhleee.itvn@gmail.com>
2023-10-06 13:41:12 +0700

    Build button | add button add text styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,40 @@
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+
+import Button from './components/Button/Button'
 
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline text-primary">
-      Hello world!
-    </h1>
-  )
+	return (
+		<div id='App' className='p-2'>
+			<h2>Button</h2>
+			<div className='flex mb-8'>
+				<Button>Primary</Button>
+				<Button secondary>Secondary</Button>
+				<Button outlined>Outlined</Button>
+				<Button text onClick={() => console.log('hello')}>
+					Text
+				</Button>
+				<Button
+					icon={<MagnifyingGlassIcon className='h-5 w-5 font-bold' />}
+				/>
+			</div>
+			<hr />
+			<h2>Texts</h2>
+			<div className='flex flex-col gap-y-4'>
+				<h1>Heading 1</h1>
+				<h2>Heading 2</h2>
+				<h3>Heading 3</h3>
+				<h4>Heading 4</h4>
+				<h5>Heading 5</h5>
+				<h6>Heading 6</h6>
+				<p>
+					Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+					Exercitationem repellat tempore corrupti deserunt illo ducimus
+					numquam tenetur pariatur provident, ea culpa voluptas consequatur
+					repellendus dolorum laborum quae quas magni id.
+				</p>
+			</div>
+		</div>
+	)
 }
 
 export default App
---
+++ client/src/components/Button/Button.jsx
@@ -0,0 +1,49 @@
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
+
+import './Button.css'
+
+function Button(props) {
+	const {
+		children,
+		className,
+		text,
+		secondary,
+		outlined,
+		icon,
+		disabled,
+		onClick = () => {},
+	} = props
+
+	const btnClasses = classNames(
+		'btn',
+		{
+			text,
+			secondary,
+			outlined,
+			disabled,
+			circle: icon,
+		},
+		className
+	)
+
+	return (
+		<button className={btnClasses} onClick={disabled ? () => {} : onClick}>
+			{icon && icon}
+			{children}
+		</button>
+	)
+}
+
+export default Button
+
+Button.propTypes = {
+	children: PropTypes.node,
+	icon: PropTypes.node,
+	secondary: PropTypes.bool,
+	outlined: PropTypes.bool,
+	text: PropTypes.bool,
+	disabled: PropTypes.bool,
+	className: PropTypes.string,
+	onClick: PropTypes.func,
+}

bad53c66906857edc398b1450dd4060ef5223b2d
dev_quynh <nhuquynh06032002@gmail.com>
2023-10-14 16:19:03 +0700

    config axios

---
+++ client/src/config/Apis.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import cookie from "react-cookies";
+
+const SERVER = "https://partnerup-api.azurewebsites.net";
+
+
+export const endpoints = {
+  companies: `${SERVER}/api/companies?pageIndex=1&pageSize=10/`,
+};
+
+export const authApi = () => {
+    return axios.create({
+        baseURL: SERVER,
+        headers: {
+            "Authorization":  cookie.load("token"),
+            
+        }
+    })
+}
+
+export default axios.create({
+    baseURL: SERVER
+})

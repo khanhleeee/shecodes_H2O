159375f228cacb956816bc6399686ea6c2de1fed
tienhuynh-tn <tien.huynhlt.tn@gmail.com>
2023-10-14 23:18:12 +0700

    [tienhuynh-tn] Filter companies by budget

--- server/src/main/java/com/shecodes2023/h2o/server/repository/CompanyRepository.java
+++ server/src/main/java/com/shecodes2023/h2o/server/repository/CompanyRepository.java
@@ -13,30 +13,31 @@ import java.util.List;
 @Repository
 public interface CompanyRepository extends JpaRepository<CompanyInfoEntity, Integer> {
 
-    @Query(value = "select * " +
-            "from (SELECT ci.accountid, " +
-            "             ci.name, " +
-            "             CAST(ci.logo AS VARCHAR(MAX))          as logo, " +
-            "             ci.province, " +
-            "             ci.budget, " +
-            "             CAST(ci.description AS NVARCHAR(MAX))  AS Description, " +
-            "             ci.establishedyear, " +
-            "             ci.createddate, " +
-            "             c.name                                 AS CategoryName, " +
-            "             s.name                                 AS ServiceName, " +
-            "             DENSE_RANK() OVER (ORDER BY AccountId) AS Rank " +
-            "      from CompanyInfo ci " +
-            "               join CompanyCategory cc on ci.AccountId = cc.CompanyId " +
-            "               join Service s on cc.ServiceId = s.Id " +
-            "               join Category c on s.CategoryId = c.Id " +
-            "      where (:province is null or ci.province = CONVERT(NVARCHAR(50), :province)) " +
-            "         or (:strListCategories is null or c.Name in (SELECT value FROM STRING_SPLIT((:strListCategories), ','))) " +
-            "          and AccountId in (select cc.CompanyId " +
-            "                            from CompanyCategory cc " +
-            "                            where cc.ServiceId in (select ss.Id " +
-            "                                                   from Service ss " +
-            "                                                   where (:strListServices is null or ss.Name in " +
-            "                                                                                      (SELECT value FROM STRING_SPLIT((:strListServices), ',')))))) r " +
+    @Query(value = "select *\n" +
+            "from (SELECT ci.accountid,\n" +
+            "             ci.name,\n" +
+            "             CAST(ci.logo AS VARCHAR(MAX))          as logo,\n" +
+            "             ci.province,\n" +
+            "             ci.budget,\n" +
+            "             CAST(ci.description AS NVARCHAR(MAX))  AS Description,\n" +
+            "             ci.establishedyear,\n" +
+            "             ci.createddate,\n" +
+            "             c.name                                 AS CategoryName,\n" +
+            "             s.name                                 AS ServiceName,\n" +
+            "             DENSE_RANK() OVER (ORDER BY AccountId) AS Rank\n" +
+            "      from CompanyInfo ci\n" +
+            "               join CompanyCategory cc on ci.AccountId = cc.CompanyId\n" +
+            "               join Service s on cc.ServiceId = s.Id\n" +
+            "               join Category c on s.CategoryId = c.Id\n" +
+            "      where (:province is null or ci.province = CONVERT(NVARCHAR(50), :province))\n" +
+            "        and (:strListCategories is null or c.Name in (SELECT value FROM STRING_SPLIT((:strListCategories), ',')))\n" +
+            "        and ((:minBudget is null and :maxBudget is null) or ci.Budget between coalesce(:minBudget, 0) and :maxBudget)\n" +
+            "        and AccountId in (select cc.CompanyId\n" +
+            "                          from CompanyCategory cc\n" +
+            "                          where cc.ServiceId in (select ss.Id\n" +
+            "                                                 from Service ss\n" +
+            "                                                 where (:strListServices is null or ss.Name in\n" +
+            "                                                                                    (SELECT value FROM STRING_SPLIT((:strListServices), ',')))))) r\n" +
             "WHERE Rank BETWEEN (:pageSize * :pageIndex - :pageSize + 1) AND (:pageSize * :pageIndex)", nativeQuery = true)
     List<CustomCompanyDTO> findAllCompanies(@Param("province") String province,
                                             @Param("strListCategories") String strListCategories,
